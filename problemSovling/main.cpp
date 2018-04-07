098b3a6a3462d3f0c881b3451c13b1313c91619a
Abdelrhman98 <pl2.buddies@gmail.com>
2018-04-07 23:54:24 +0200

    first repos

---
+++ problemSovling/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: bodey
+ *
+ * Created on April 7, 2018, 11:53 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
